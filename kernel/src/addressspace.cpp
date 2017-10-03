29df19957a5ffcc159e4589a9b4cc988a27ffb85
Qv Junping <qjp5555@gmail.com>
2017-10-03 22:04:23 +0800

    Add AddressSpace::map() function.

--- kernel/src/addressspace.cpp
+++ kernel/src/addressspace.cpp
@@ -1,6 +1,6 @@
-#include <stddef.h>
 #include <inlow/kernel/addressspace.h>
 #include <inlow/kernel/kernel.h>
+#include <inlow/kernel/print.h>
 
 #define RECURSIVE_MAPPING 0xFFC00000
 
@@ -12,6 +12,17 @@ extern "C"
 
 AddressSpace AddressSpace::_kernelSpace;
 AddressSpace* kernelSpace;
+
+static inline vaddr_t indexToAddress(size_t pdIndex, size_t ptIndex)
+{
+	return (pdIndex << 22) | (ptIndex << 12);
+}
+
+static inline void addressToIndex(vaddr_t virtualAddress, size_t& pdIndex, size_t& ptIndex)
+{
+	pdIndex = virtualAddress >> 22;
+	ptIndex = (virtualAddress >> 12) & 0x3FF;
+}
 
 AddressSpace::AddressSpace()
 {
@@ -31,26 +42,58 @@ void AddressSpace::initialize()
 	}
 }
 
+vaddr_t AddressSpace::map(paddr_t physicalAddress, int flags)
+{
+	// Find free page from the higher half amd map it
+	for (size_t pdIndex = 0x300; pdIndex < 0x400; pdIndex++)
+	{
+		for (size_t ptIndex = 0; ptIndex < 0x400; ptIndex++)
+		{
+			uintptr_t* pageEntry = (uintptr_t*)(RECURSIVE_MAPPING + 0x1000 * pdIndex + 4 * ptIndex);
+			if (!*pageEntry)
+			{
+				return mapAt(pdIndex, ptIndex, physicalAddress, flags);
+			}
+		}
+	}
+	return 0;
+}
 vaddr_t AddressSpace::mapAt(vaddr_t virtualAddress, paddr_t physicalAddress, int flags)
 {
-	size_t pdIndex = virtualAddress >> 22;
-	size_t ptIndex = (virtualAddress >> 12) & 0x3FF;
-
+	size_t pdIndex; 
+	size_t ptIndex;
+	addressToIndex(virtualAddress, pdIndex, ptIndex);
+	return mapAt(pdIndex, ptIndex, physicalAddress, flags);
+}
+vaddr_t AddressSpace::mapAt(size_t pdIndex, size_t ptIndex, paddr_t physicalAddress, int flags)
+{
 	if(this == kernelSpace)
 	{
-		uintptr_t* pageEntry = (uintptr_t*)(RECURSIVE_MAPPING + 0x1000 * pdIndex + 4 * ptIndex);
-		*pageEntry = physicalAddress | flags;
+		uintptr_t* pageTable = (uintptr_t*)(RECURSIVE_MAPPING + 0x3FF000 + 4 * pdIndex);
+		if (*pageTable)
+		{
+			uintptr_t* pageEntry = (uintptr_t*)(RECURSIVE_MAPPING + 0x1000 * pdIndex + 4 * ptIndex);
+			*pageEntry = physicalAddress | flags;
+		}
+		else
+		{
+			//TODO: Allocate a new page table;
+				Print::printf("ERROR: Page Table does not exist.\n");
+				return 0;
+		}
 	}
 	else
 	{
 		//TODO: Implement this for other address spaces.
+		return 0;
 	}
+	vaddr_t virtualAddress = indexToAddress(pdIndex, ptIndex);
 
 	// Flush the TLB
 	asm volatile ("invlpg (%0)" :: "r"(virtualAddress));
-
-	return virtualAddress;
 }
+
+
 
 void AddressSpace::unmap(vaddr_t virtualAddress)
 {
