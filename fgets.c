f07aa6b36f8bcaa7f3fe62c829c90c1545b82faa
Qv Junping <qjp5555@gmail.com>
2017-10-19 20:23:37 +0800

    rm mistake file

--- fgets.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-char* fgets(char restrict buffer, int size, FILE* restrict file)
-{
-	flockfile(file);
-	char* result = fgets_unlocked(buffer, size, file);
-	funlockfile(file);
-	return result;
-}
