3b8db9e9fc21c4d8aa7f554a50958b53f84e5cce
Qv Junping <qjp5555@gmail.com>
2017-10-26 14:50:30 +0800

    Fix a bug & Refactor lib/Makefile

--- kernel/include/inlow/kernel/syscall.h
+++ kernel/include/inlow/kernel/syscall.h
@@ -9,16 +9,16 @@
 struct __mmapRequest;
 namespace Syscall
 {
+	int close(int fd);
 	int execve(const char* path, char* const argv[], char* const envp[]);
 	NORETURN void exit(int status);
-	ssize_t read(int fd, void* buffer, size_t size);
-	ssize_t write(int fd, const void* buffer, size_t size);
 	void* mmap(__mmapRequest* request);
 	int munmap(void* addr, size_t size);
 	int openat(int fd, const char* path, int flags, mode_t mode);
-	int close(int fd);
+	ssize_t read(int fd, void* buffer, size_t size);
 	pid_t regfork(int flags, struct regfork* registers);
 	pid_t waitpid(pid_t pid, int* status, int flags);
+	ssize_t write(int fd, const void* buffer, size_t size);
 	
 	void badSyscall();
 }
