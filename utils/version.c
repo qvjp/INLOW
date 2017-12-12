af221164507e84ed96342c66aab9cddde7ab71c1
Qv Junping <qjp5555@gmail.com>
2017-12-12 23:23:51 +0800

    Update "version"

--- utils/version.c
+++ utils/version.c
@@ -2,61 +2,69 @@
 #include <stdlib.h>
 #include <string.h>
 
-static char* image;
-static int x, y, w, h, n;
-
-static void Put(int i, int j, char c) {
-    char *p = image + j * w + i;
-    if (*p == ' ' || *p == '_')
-        *p = c;
-}
-
-static void KochCurve(int n, int dir) {
-    if (n == 0)
-        switch (dir % 6) {
-            case 0: Put(x++, y  , '_' );
-                    Put(x++, y,   '_' ); break;
-            case 1: Put(x++, y--, '/' ); break;
-            case 2: Put(--x, y--, '\\'); break;
-            case 3: Put(--x,   y, '_' );
-                    Put(--x,   y, '_' ); break;
-            case 4: Put(--x, ++y, '/' ); break;
-            case 5: Put(x++, ++y, '\\'); break;
-        }
-    else {
-        KochCurve(n - 1, dir    );
-        KochCurve(n - 1, dir + 1);
-        KochCurve(n - 1, dir + 5);
-        KochCurve(n - 1, dir    );
-    }
-}
+#define W 80
+#define H 25
+#define NONE          "\e[m"
+#define RED           "\e[0;41m"
+#define GREEN         "\e[0;42m"
+#define BROWN         "\e[0;43m"
+#define BLUE          "\e[0;44m"
+#define PURPLE        "\e[0;45m"
+#define CYAN          "\e[0;46m"
+#define WHITE         "\e[0;47m"
 
 
-int main() {
-    w = 25;
-    h = 15;
-	n = 2;
-    image = (char*)malloc(w * h);
-    memset(image, ' ', w * h);
+char* color[] = {GREEN, BROWN, BLUE, PURPLE, CYAN, WHITE};
 
-    x = 5;
-    y = h / 4 - 1;
-    KochCurve(n, 0);
-    KochCurve(n, 4);
-    KochCurve(n, 2);
-
+int main(int argc, char* argv[])
+{
+	(void) argc;
+	(void) argv;
+	int x = 20;
+	int y = 6;
 	printf("\e[H\e[2J");
-	printf("\n");
-    for (int j = 0; j < h; j++) {
-        for (int i = 0; i < w; i++)
-            putchar(image[j * w + i]);
-        putchar('\n');
+    for (int j = 0; j < H; j++) {
+        for (int i = 0; i < W; i++)
+		{
+			printf(color[j%6]);
+			printf(" ");
+		}
     }
+	printf("\e[41m");
+	for (int i = 0; i < 7; i++)				// I
+		printf("\e[%d;%dH ", y+i, x);
 
-    w *= 3;
-    h = n == 0 ? 4 : h * 3;
-    free(image);
-	printf("INLOW 0.0.2 SNOW\n\n");
+	for (int i = 0; i < 7; i++)				// N
+		printf("\e[%d;%dH ", y+i, x+3);
+	for (int i = 0; i < 6; i++)
+		printf("\e[%d;%dH ", y+i, x+3+i);
+	for (int i = 0; i < 7; i++)
+		printf("\e[%d;%dH ", y+i, x+3+6);
+	
+	for (int i = 0; i < 7; i++)
+		printf("\e[%d;%dH ", y + i, x + 3 + 6 + 3);	// L
+	for (int i = 0; i < 7; i++)
+		printf("\e[%d;%dH ", y+6, x + 3 + 6 + 3 + i);
+
+	for (int i = 0; i < 5; i++)							// O
+		printf("\e[%d;%dH ", y+1+i, x + 3+ 6+3+6+3);
+	for (int i = 0; i < 5; i++)
+		printf("\e[%d;%dH ", y, x + 3+ 6+3+6+3+1+i);
+	for (int i = 0; i < 5; i++)
+		printf("\e[%d;%dH ", y+6, x + 3+ 6+3+6+3+1+i);
+	for (int i = 0; i < 5; i++)
+		printf("\e[%d;%dH ", y+1+i, x + 3+ 6+3+6+3+6);
+
+	for (int i = 0; i < 7; i++)							//W
+		printf("\e[%d;%dH ", y+i, x + 3 + 6 + 3 + 6 + 3 + 6 +3);
+	for (int i = 0; i < 3; i++)
+		printf("\e[%d;%dH ", y+5-i, x + 3+ 6+3+6+3+6+3+1+i);
+	for (int i = 0; i < 2; i++)
+		printf("\e[%d;%dH ", y+4+i, x + 3+ 6+3+6+3+6+3+4+i);
+	for (int i = 0; i < 7; i++)
+		printf("\e[%d;%dH ", y+i, x + 3 + 6 + 3 + 6 + 3 + 6 +3+6);
+		
+	printf("\e[16;50H\e[1;32;45mINLOW 0.0.3 LIGHT");
+	printf("\e[25;1H\e[0m");
+    return 0;
 }
-
-/*Thanks for Milo Yip*/
