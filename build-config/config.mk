50a4eea9019ad99f4dde0fe994aa18f184b31e3d
Qv Junping <qvjunping@gmail.com>
2018-06-13 17:01:34 +0800

    开启分页
    开启分页，将内核映射到0xC0000000，
    实现所谓"higher-half kernel"

--- build-config/config.mk
+++ build-config/config.mk
@@ -2,6 +2,7 @@ ARCH := i686
 BUILD_DIR = $(TO_ROOT)/build/$(ARCH)
 GCC := $(ARCH)-elf-gcc
 
+ASFLAGS += -Wa,--divide
 CFLAGS ?= -O2 -g
 CFLAGS += -std=gnu99 -ffreestanding -fno-exceptions -Wall -Wextra -I $(TO_ROOT)/kernel/include
 LDFLAGS ?= -T $(TO_ROOT)/kernel/src/linker.ld -ffreestanding -nostdlib
--- kernel/src/arch/i686/loader.s
+++ kernel/src/arch/i686/loader.s
@@ -15,16 +15,76 @@
     .long MB_FLAGS
     .long MB_CHECKSUM
 
-/* 这个段包含的数据在内核加载时会被初始化为0 */
+/* 下边两个section是启用分页之前，以地址0x100000运行的代码 */
+/* 存放页目录和页表 */
+.section bootstrap_bss, "aw", @nobits
+    .align 4096
+    pageDirectory:
+        .skip 4096
+    bootstrapPageTable:
+        .skip 4096
+    kernelPageTable:
+        .skip 4096
+
+/* 设置页，跳到高地址内核 */
+.section bootstrap_text, "ax"
+    _start:
+        # 添加页表到页目录
+        movl $(bootstrapPageTable + 0x3), pageDirectory
+        movl $(kernelPageTable + 0x3), pageDirectory + 0xC00
+
+        # 恒等映射(identity mapping)bootstrap section
+        mov $numBootstrapPages, %ecx
+        mov $(bootstrapPageTable + 256 * 4), %edi
+        mov $(bootstrapBegin + 3), %edx
+    1:
+        mov %edx, (%edi)
+        # bootstrapPageTable中入口大小为4 bytes
+        add $4, %edi
+        # 页大小为4096 bytes
+        add $4096, %edx
+        # 循环直到最后一个入口
+        loop 1b
+
+        # 映射内核
+        mov $numKernelPages, %ecx
+        add $(kernelPageTable - bootstrapPageTable), %edi
+        mov $(kernelPhysicalBegin + 3), %edx
+    1:
+        mov %edx, (%edi)
+        # kernelPageTable中入口大小为4 bytes
+        add $4, %edi
+        # 页大小为4096 bytes
+        add $4096, %edx
+        # 循环直到最后一个入口
+        loop 1b
+
+        # 映射视频内存
+        movl $0xB8003, bootstrapPageTable + 0x2E0
+
+        /* 开启分页 */
+        mov $pageDirectory, %ecx
+        mov %ecx, %cr3
+
+        mov %cr0, %ecx       /* 读取当前cr0 */
+        or $0x80000000, %ecx /* 设置PG */
+        mov %ecx, %cr0       /* 更新cr0 */
+
+        # 跳转到高地址空间
+        jmp _start_high
+.size _start, . - _start
+
+/* 下边两个section是启用分页之后代码所在位置 */
+/* bss包含的数据在内核加载时会被初始化为0 */
 .section .bss
     .align 16
     stack_bottom:
         .skip 4096
     stack_top:
 
-/* 这个段包含真正要运行的代码 */
+/* text是可执行代码所放位置 */
 .section .text
-    _start:
+    _start_high:
         /* 加载GDT */
         mov $gdt_descriptor, %ecx     /* 加载GDT的地址 */
         lgdt (%ecx)                  /* 加载GDT */
@@ -55,4 +115,4 @@
             hlt
             jmp hang
 
-.size _start, . - _start
+.size _start_high, . - _start_high
